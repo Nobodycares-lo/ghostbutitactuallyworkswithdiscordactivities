055b1e4bab0643836fc401cda1791b336c41174d
Nobodycares-lo <admin@nobodycareslo.me>
2024-12-27 15:45:37 -0800

    fix api issues

--- public/js/weirdahhscript.js
+++ public/js/weirdahhscript.js
@@ -1,10 +1,6 @@
-let rgAPI = '/assets/rg.json'
+let rgAPI = [{"name":"There is No Game","img":"/assets/img/assets/thereisnogame.png","href":"https://game258367.konggames.com/gamez/0025/8367/live/index.html"},{"name":"pulsus","img":"/assets/img/assets/pulsus.png","href":"https://www.pulsus.cc/play/"},{"name":"Bad time simulator","img":"/assets/img/assets/badtimesimulator.png","href":"https://jcw87.github.io/c2-sans-fight"},{"name":"Hobo 4","img":"/assets/img/assets/hobo4.png","href":"https://www.gameflare.com/embed/hobo-4/"},{"name":"Baldi's Basics","img":"/assets/img/assets/baldi.png","href":"https://healthstudies.commwebworks.com/game/baldi/index.html"},{"name":"Hobo 6","img":"/assets/img/assets/hobo6.png","href":"https://games.crazygames.com/en_US/hobo-6/index.html?v=1.301"},{"name":"Backrooms","img":"/assets/img/assets/backrooms.png","href":"https://backroomsgame.io/game/backrooms/"},{"name":"Cookie Clicker","img":"/assets/img/assets/cookie1.jpeg","href":"https://orteil.dashnet.org/cookieclicker/"}]
 addEventListener('DOMContentLoaded', async (event) => {
-    const gData = await fetch(rgAPI)
-        .then((response) => response.text())
-        .then((text) => {
-            return JSON.parse(text)
-        })
+    const gData = JSON.parse(rgAPI)
     gData.forEach((game) => {
         const i = document.getElementById('trendingg')
         const g = document.createElement('div')
@@ -30,13 +26,9 @@ addEventListener('DOMContentLoaded', async (event) => {
         i.appendChild(g)
     })
 })
-let raAPI = '/assets/ra.json'
+let raAPI = [{"name":"Twitch","img":"/assets//img/apps/twitch.png","href":"https://twitch.tv"},{"name":"Spotify","img":"/assets//img/apps/spotify2.png","href":"https://spotify.com"},{"name":"Duck Duck Go","img":"/assets//img/apps/ddg.png","href":"https://duckduckgo.com"},{"name":"Soundcloud","img":"/assets/img/assets/soundcloud.png","href":"https://soundcloud.com"},{"name":"Amazon","img":"/assets//img/apps/amazon.png","href":"https://amazon.com"},{"name":"Amazon Luna","img":"/assets//img/apps/luna.png","href":"https://luna.amazon.com"},{"name":"Pintrest","img":"/assets//img/apps/pintrest.png","href":"https://pinterest.com"},{"name":"Xbox Cloud Gaming","img":"/assets//img/apps/xbox.png","href":"https://xbox.com/play"}]
 addEventListener('DOMContentLoaded', async (event) => {
-    const gData = await fetch(raAPI)
-        .then((response) => response.text())
-        .then((text) => {
-            return JSON.parse(text)
-        })
+    const gData = JSON.parse(raAPI)
     gData.forEach((game) => {
         const i = document.getElementById('trendinga')
 
